2c41d5f5323027268f27d1995dc97b22bf13db9e
Noah Streller <noah.streller@gmx.ch>
2024-03-08 15:59:29 +0100

    wip: added categories

--- app/(tabs)/home/index.tsx
+++ app/(tabs)/home/index.tsx
@@ -1,9 +1,11 @@
 import { Text, View } from 'react-native';
+import CategoryDropdown from '../../../components/category-dropdown';
 
 export default function Home() {
   return (
     <View >
       <Text>Open up App.js to start working on your app!</Text>
+      <CategoryDropdown />
     </View>
   );
 }
---
+++ components/category-dropdown.tsx
@@ -0,0 +1,18 @@
+
+export default function CategoryDropdown(){
+  const categories = [
+    "family",
+    "office",
+    "children",
+    "college",
+    "party",
+    "funny",
+    "unbelievable",
+    "developers",
+    "gaming"
+  ];
+
+  return (
+    null
+  )
+}
