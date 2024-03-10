1872e1ba1d9641af4df3058140e02a7e9cca9f73
Noah Streller <noah.streller@gmx.ch>
2024-03-10 08:06:30 +0100

    feat: add dropdown

--- components/category-dropdown.tsx
+++ components/category-dropdown.tsx
@@ -1,18 +1,33 @@
+import { ReactNode, useState } from "react";
+import DropDown from "react-native-paper-dropdown";
 
 export default function CategoryDropdown(){
-  const categories = [
-    "family",
-    "office",
-    "children",
-    "college",
-    "party",
-    "funny",
-    "unbelievable",
-    "developers",
-    "gaming"
+  
+  const categories: Category[] = [
+    { label: "Family", value: "family" },
+    { label: "Office", value: "office" },
+    { label: "Children", value: "children" },
+    { label: "College", value: "college" },
+    { label: "Party", value: "party" },
+    { label: "Funny", value: "funny" },
+    { label: "Unbelievable", value: "unbelievable" },
+    { label: "Developers", value: "developers" },
+    { label: "Gaming", value: "gaming" }
   ];
 
+  const [value, setValue] = useState<Category>();
+  const [showDropDown, setShowDropDown] = useState<boolean>(false);
+
   return (
-    null
+    <DropDown 
+      onDismiss={()=>setShowDropDown(false)}
+      visible={showDropDown}
+      value={value}
+      setValue={setValue} 
+      showDropDown={()=>setShowDropDown(true)} 
+      list={categories}      
+    />
   )
 }
+
+export type Category = { label: string; value: string | number; custom?: ReactNode; }
